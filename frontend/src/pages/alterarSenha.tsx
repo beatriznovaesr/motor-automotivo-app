fb9d1daf281e7f2695c2bf57423beca948a54f5d
Natalia Yukari <nataliakawashisa@gmail.com>
2025-05-19 16:39:01 -0300

    All files checked

---
+++ frontend/src/pages/alterarSenha.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { View } from "react-native";
+import { Input } from '../components/input';
+import { Button } from '../components/button';
+import { PageTitle } from "../components/pageTitle";
+
+export default function TelaAlterarSenha() {
+
+    const handleNovaSenha = async () => {}
+
+    return (
+                <View
+                    style={{
+                    flex: 1,
+                    backgroundColor: '#155fbf',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '100%',
+                    gap: 5,
+                    marginVertical: 10
+                }}>
+                    <PageTitle text='Alterar senha'></PageTitle>
+                    <Input title='Senha atual: '></Input>
+                    <Input title='Nova senha: '></Input>
+                    <Input title='Confirme a nova senha: '></Input>
+                    <Button text='Salvar nova senha' onPress={handleNovaSenha}></Button>
+                </View>
+    );
+}
