cb28eeb0bd7f6367a0b3ac178b426a3ac26a7ba6
Natalia Yukari Kawashisa <nataliakawashisa@gmail.com>
2025-07-04 14:31:10 -0300

    Adicionando comp de erro de conexão à tela inicial

--- frontend/app/telaInicial.tsx
+++ frontend/app/telaInicial.tsx
@@ -6,6 +6,7 @@ import { Input } from '../src/components/input';
 import { Button } from '../src/components/button';
 import  Imagem  from '../src/components/image';
 import { NavigationMenu } from '../src/components/navigationMenu/navigationMenu';
+import ConnectionErrorModal from '../src/components/connectionError/ConnectionErrorModal';
 
 export default function TelaInicial() {
     const router = useRouter();
@@ -13,6 +14,7 @@ export default function TelaInicial() {
     const [loading, setLoading] = useState(false);
     const [jaPesquisou, setJaPesquisou] = useState(false);
     const [nenhumResultado, setNenhumResultado] = useState(false);
+    const [isModalVisible, setModalVisible] = useState(false);
 
     useFocusEffect(
         useCallback(() => {
@@ -57,8 +59,13 @@ export default function TelaInicial() {
 
         }  catch (error) {
             console.error(error);
-            Alert.alert("Erro", "Não foi possível se conectar ao servidor");
+            setModalVisible(true);
+            //Alert.alert("Erro", "Não foi possível se conectar ao servidor");
         }
+    };
+
+    const closeModal = () => {
+        setModalVisible(false);
     };
 
     return (
@@ -87,7 +94,7 @@ export default function TelaInicial() {
                     <Imagem source={require('./assets/logo.png')} width={150} height={150} borderRadius={60}></Imagem>
                     <Input title='Pesquise por motor, montadora ou carro: ' variant='mainScreen' value={palavraPesquisada} onChangeText={setPalavraPesquisada}></Input>
                     <Button text='Pesquisar' onPress={handlePesquisar}></Button>
-
+                    <ConnectionErrorModal visible={isModalVisible} onClose={closeModal}/>
                     <NavigationMenu/>
                 </View>
     );
